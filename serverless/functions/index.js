6719de1925a5f15ccf2e09ed065b5678dadad79f
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2017-12-09 19:17:08 +0200

    feat(serveless): save command to db

--- serverless/functions/index.js
+++ serverless/functions/index.js
@@ -1,11 +1,33 @@
 const functions = require('firebase-functions');
 const express = require("express");
 const cors = require("cors");
+const moment = require("moment");
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+const updateDatabase = (command) => {
+    const date = moment().format();
+    return new Promise((res) => {
+        const queueRef = admin.database().ref('/queue');
+
+        queueRef
+            .push({date, command})
+            .then(res);
+    });
+};
 
 const app = express();
-
 app.use(cors());
-app.get('/start', (req, res) => res.send("starting movie night"));
-app.get('/stop', (req, res) => res.send("stopping movie night"));
+
+app.get('/start', (req, res) => {
+    updateDatabase("start").then(() =>
+        res.send("starting movie night"));
+});
+
+app.get('/stop', (req, res) => {
+    updateDatabase("stop").then(() =>
+        res.send("stopping movie night"));
+});
+
 
 exports.movieMode = functions.https.onRequest(app);
