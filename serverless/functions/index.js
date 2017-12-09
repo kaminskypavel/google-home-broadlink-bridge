d1999bc0bb446301486aa3f7a2e3a2c1ea45e12e
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2017-12-09 18:34:23 +0200

    feat(serveless): integrate express.js

--- serverless/functions/index.js
+++ serverless/functions/index.js
@@ -1,8 +1,11 @@
 const functions = require('firebase-functions');
+const express = require("express");
+const cors = require("cors");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-    response.send("Hello Roboto!");
-});
+const app = express();
+
+app.use(cors());
+app.get('/start', (req, res) => res.send("starting movie night"));
+app.get('/stop', (req, res) => res.send("stopping movie night"));
+
+exports.movieMode = functions.https.onRequest(app);
