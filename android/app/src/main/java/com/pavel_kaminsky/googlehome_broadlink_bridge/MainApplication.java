4817a0bd1c60767767ae03a01d5f2a6c089b4402
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2017-12-09 16:53:13 +0200

    feat(android) : integrate firebase

---
+++ android/app/src/main/java/com/pavel_kaminsky/googlehome_broadlink_bridge/MainApplication.java
@@ -0,0 +1,39 @@
+package com.pavel_kaminsky.googlehome_broadlink_bridge;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import static android.content.ContentValues.TAG;
+
+public class MainApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+    }
+}
